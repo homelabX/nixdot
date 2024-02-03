c9598d6e1076e77c9d436675893b34b67d0e3484
homelabX <140975542+homelabX@users.noreply.github.com>
2024-02-03 17:57:53 +0530

    new flake lock

--- flake.nix
+++ flake.nix
@@ -1,40 +1,46 @@
 {
-  description = "NixOS Configuration";
+  description = "nambolan's nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixOS/nixpkgs/nixos-unstable";
-
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { home-manager, nixpkgs, ...}@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      lib = nixpkgs.lib;
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    lib = nixpkgs.lib // home-manager.lib;
+    systems = ["x86_64-linux"];
+    forEachSystem = f: lib.genAttrs systems (sys: f pkgsFor.${sys});
+    pkgsFor = nixpkgs.legacyPackages;
+  in {
+    inherit lib;
+    nixosModules = import ./modules/nixos;
+    homeManagerModules = import ./modules/home-manager;
+    overlays = import ./overlays {inherit inputs outputs;};
+    packages = forEachSystem (pkgs: import ./pkgs {inherit pkgs;});
+    devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs inputs;});
 
-      in {
-        nixosModules = import ./modules/nixos;
-        homeManagerModules = import ./modules/home-manager;
-       
-        nixosConfigurations = {
-          precision = lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
-          modules = [
-            ./nixos/configuration.nix
-          ];
-        };
-
-        homeConfigurations = {
-          giri = home-manager.lib.homeManagerConfiguration {
-          pkgs = pkgs;
-          extraSpecialArgs = { inherit inputs outputs; };
-          modules = [
-            ./home-manager/home.nix
-          ];
-        };
+    nixosConfigurations = {
+      precision = lib.nixosSystem {
+        modules = [./hosts/precision/configuration.nix];
+        specialArgs = {inherit inputs outputs;};
       };
- }
+    };
+
+    homeConfigurations = {
+      precision = lib.homeManagerConfiguration {
+        modules = [./hosts/precision/home.nix];
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        extraSpecialArgs = {inherit inputs outputs;};
+      };
+    };
+  };
+}
