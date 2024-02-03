c8d3e95539734258cf95f3ff3a1fb0cefd1fdecc
homelabX <140975542+homelabX@users.noreply.github.com>
2024-02-03 21:46:35 +0530

    add default env vars for wayland

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -13,6 +13,12 @@ in {
       hyprland
     ];
 
+    home.sessionVariables = {
+      MOZ_ENABLE_WAYLAND = 1;
+      QT_QPA_PLATFORM = "wayland";
+      LIBSEAT_BACKEND = "logind";
+    };
+
     home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
   };
 }
