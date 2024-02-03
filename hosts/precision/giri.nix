0efa7f4fbd9b662dee5dba38087b6b410d22841c
homelabX <140975542+homelabX@users.noreply.github.com>
2024-02-03 20:53:24 +0530

    explicit homedir

--- hosts/precision/giri.nix
+++ hosts/precision/giri.nix
@@ -5,16 +5,20 @@
     ../../modules/default.nix 
   ];
 
-  config.modules = {
-    zsh.enable = true;
-    hyprland.enable = false;
-    mpd.enable = true;
-    mpd.runas = "giri";
-  };
+  config = {
+    modules = {
+      zsh.enable = true;
+      hyprland.enable = false;
+      mpd.enable = true;
+      mpd.runas = "giri";
+    };
 
-  home = {
-    username = "giri";
-    homeDirectory = "/home/giri";
-    stateVersion = "23.11";
-  };
+    home = {
+      username = "giri";
+      homeDirectory = "/home/giri";
+      stateVersion = "23.11";
+    };
+  }
+
+
 }
