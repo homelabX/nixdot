dbebafd95c97c5d0b8e98ca3bbfaf343d2f0117c
homelabX <140975542+homelabX@users.noreply.github.com>
2024-02-04 06:25:02 +0530

    nix settings

--- nixos/default.nix
+++ nixos/default.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     inputs.home-manager.nixosModules.home-manager
-    ./conf.nix
+    ./nix.nix
     ./pipewire.nix
     ./fonts.nix
   ];
---
+++ nixos/nix.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+
+{ 
+  nix = {
+    settings = {
+      trusted-users = [ "root" "@wheel" ];
+      auto-optmize-store = lib.mkDefault true;
+      use-xdg-base-directories = true;
+      warn-dirty = false;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 2d";
+    };
+  };
+}
+
