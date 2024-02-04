b1dc9dc4e5d93ef91b615905d4996359416469d1
homelabX <140975542+homelabX@users.noreply.github.com>
2024-02-04 06:19:05 +0530

    vscode extensions

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -10,6 +10,20 @@ in {
     programs = {
       vscode = {
         enable = true;
+        extensions = with pkgs.vscode-extensions; [
+          bbenoist.nix
+          eamodio.gitlens
+          rust-lang.rust-analyzer
+          tamasfe.even-better-toml
+          esbenp.prettier-vscode
+          pkief.material-icon-theme
+        ];
+
+        userSettings = {
+          "workbench.colorTheme"= "Default Dark Modern";
+          "window.titleBarStyle"= "custom";
+          "workbench.iconTheme"= "material-icon-theme";
+        };
       };
     };
   };
