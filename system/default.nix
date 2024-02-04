a21ceab77625d981cae49527448671907020b35b
homelabX <140975542+homelabX@users.noreply.github.com>
2024-02-04 17:41:24 +0530

    cleanup

--- system/default.nix
+++ system/default.nix
@@ -2,15 +2,11 @@ let
   desktop = [
     ./core/boot.nix
     ./core/default.nix
-#
     #./hardware/opengl.nix
-#
     #./network/avahi.nix
     #./network/default.nix
     #./network/tailscale.nix
-#
     ./programs
-#
     #./services
     #./services/greetd.nix
     #./services/pipewire.nix
