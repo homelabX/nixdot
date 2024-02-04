663c8034e6aaf360d022dda4e987a05c75e44ab4
homelabX <140975542+homelabX@users.noreply.github.com>
2024-02-04 08:13:38 +0530

    new flake

--- flake.nix
+++ flake.nix
@@ -1,46 +1,99 @@
 {
-  description = "nambolan's nix config";
+  description = "nambolan's flake";
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+
+      imports = [
+        ./home/profiles
+        ./hosts
+        ./lib
+        ./modules
+        ./pkgs
+        ./pre-commit-hooks.nix
+      ];
+
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            alejandra
+            git
+          ];
+          name = "dots";
+          DIRENV_LOG_FORMAT = "";
+          shellHook = ''
+            ${config.pre-commit.installationScript}
+          '';
+        };
+
+        formatter = pkgs.alejandra;
+      };
+    };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    fu.url = "github:numtide/flake-utils";
+
+    helix.url = "github:helix-editor/helix";
+
+    hm = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    lib = nixpkgs.lib // home-manager.lib;
-    systems = ["x86_64-linux"];
-    forEachSystem = f: lib.genAttrs systems (sys: f pkgsFor.${sys});
-    pkgsFor = nixpkgs.legacyPackages;
-  in {
-    inherit lib;
-    # nixosModules = import ./modules/nixos;
-    # homeManagerModules = import ./modules/home-manager;
-    # overlays = import ./overlays {inherit inputs outputs;};
-    # packages = forEachSystem (pkgs: import ./pkgs {inherit pkgs;});
-    # devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs inputs;});
+    hyprland.url = "github:hyprwm/Hyprland";
 
-    nixosConfigurations = {
-      precision = lib.nixosSystem {
-        modules = [./hosts/precision/configuration.nix];
-        specialArgs = {inherit inputs outputs;};
-      };
+    hyprland-contrib = {
+      url = "github:hyprwm/contrib";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    homeConfigurations = {
-      "giri@precision"= lib.homeManagerConfiguration {
-        modules = [./hosts/precision/giri.nix];
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = {inherit inputs outputs;};
-      };
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
     };
+
+    hyprpaper.url = "github:hyprwm/hyprpaper";
+
+    kmonad = {
+      url = "github:kmonad/kmonad?dir=nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    lanzaboote.url = "github:nix-community/lanzaboote";
+
+    matugen = {
+      url = "github:InioX/matugen/module";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nh = {
+      url = "github:viperML/nh";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-index-db = {
+      url = "github:Mic92/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "fu";
+    };
+
+    yazi.url = "github:sxyazi/yazi";
   };
 }
