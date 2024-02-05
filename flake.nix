78e6cd8db593cfc75816e50e322701a3b45f98a2
homelabX <140975542+homelabX@users.noreply.github.com>
2024-02-05 11:06:48 +0530

    updated nix inputs

--- flake.nix
+++ flake.nix
@@ -7,6 +7,9 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    hyprland-nix.url = "github:spikespaz/hyprland-nix";
+    nix-colors.url = "github:misterio77/nix-colors";
   };
 
   outputs = {
@@ -23,7 +26,7 @@
   in {
     inherit lib;
     # nixosModules = import ./modules/nixos;
-    # homeManagerModules = import ./modules/home-manager;
+    homeManagerModules = import ./modules/home-manager;
     # overlays = import ./overlays {inherit inputs outputs;};
     # packages = forEachSystem (pkgs: import ./pkgs {inherit pkgs;});
     # devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs inputs;});
