77ce568452600e21e68a051c604c971e5ef5d618
homelabX <140975542+homelabX@users.noreply.github.com>
2024-02-05 12:38:43 +0530

    monitor check

--- hosts/precision/giri.nix
+++ hosts/precision/giri.nix
@@ -38,17 +38,17 @@
 
     colorscheme = inputs.nix-colors.colorSchemes.catppuccin-mocha;
 
-    wayland.windowManager.hyprland.settings =  {
-      monitor = [
-        "DP-3,1920x1080,0x0,1"
-        "eDP-1,1920x1080,1920x0,1"
-      ];
-    };
-
     home = {
       username = "giri";
       homeDirectory = "/home/giri";
       stateVersion = "23.11";
     };
   };
+
+  wayland.windowManager.hyprland.settings =  {
+  monitor = [
+    "DP-3,1920x1080,0x0,1"
+    "eDP-1,1920x1080,1920x0,1"
+  ];
+};
 }
