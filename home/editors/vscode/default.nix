0d44fb02e82f5b77e5d335731981a957884210f0
homelabX <140975542+homelabX@users.noreply.github.com>
2024-02-05 12:10:33 +0530

    fix imports

--- home/editors/vscode/default.nix
+++ home/editors/vscode/default.nix
@@ -4,14 +4,14 @@ with lib;
 let cfg = config.modules.editors.vscode;
 
 in {
+  imports = [
+    ./extensions.nix
+    ./user_settings.nix
+  ];
   options.modules.editors.vscode = { enable = mkEnableOption "vscode"; };
 
   config = mkIf cfg.enable {
 
-    imports = [
-      ./extensions.nix
-      ./user_settings.nix
-    ];
     programs = {
       vscode = {
         enable = true;
