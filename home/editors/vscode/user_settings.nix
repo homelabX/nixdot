bcc4c7900731cc109484f1682ab13d7066b78fbe
homelabX <140975542+homelabX@users.noreply.github.com>
2024-02-06 23:48:22 +0530

    vscode nerdfont

--- home/editors/vscode/user_settings.nix
+++ home/editors/vscode/user_settings.nix
@@ -9,7 +9,7 @@
         "workbench.iconTheme"= "material-icon-theme";
         "[nix]"."editor.tabSize" = 2;
         "telemetry.telemetryLevel" = "off";
-        "editor.fontFamily" = "'FiraCode Nerd Font', 'Droid Sans Mono', 'monspace', monospace";
+        "editor.fontFamily" = "'FiraCode Nerd Font Mono', 'Droid Sans Mono', 'monspace', monospace";
         "editor.fontLigatures" = true;
         "terminal.integrated.fontFamily" = "FiraCode Nerd Font";
       };
