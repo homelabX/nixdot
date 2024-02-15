08a874f7c92536849ac82ed80a75414e3e13e13f
homelabX <140975542+homelabX@users.noreply.github.com>
2024-02-15 19:39:55 +0530

    waybar graphical session systemd

--- home/programs/waybar/default.nix
+++ home/programs/waybar/default.nix
@@ -5,9 +5,9 @@ with lib; let
 
 in {
 
-  # imports = [
-  #   ./settings.nix
-  # ];
+  imports = [
+    ./settings.nix
+  ];
   
   options.modules.programs.waybar = {
     enable = mkEnableOption "Enable Waybar";
@@ -16,6 +16,8 @@ in {
   config = mkIf cfg.enable {
     programs.waybar = {
       enable = true;
+      systemd.enable = true;
+      systemd.target = "graphical-session-target";
     };
   };
 }
