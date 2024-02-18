922f256da0d95dfcb76d68c39dcc45dc4d285f7c
homelabX <140975542+homelabX@users.noreply.github.com>
2024-02-18 09:03:56 +0530

    add sudo while mounting

--- hosts/precision/shell_helpers/unlock_data.sh
+++ hosts/precision/shell_helpers/unlock_data.sh
@@ -1,7 +1,7 @@
 function unlock_data() {
     mkdir -p /data
     sudo cryptsetup luksOpen /dev/disk/by-uuid/b796945e-efcc-4ee7-b34a-ace3c7e4766e data
-    mount /dev/mapper/data /data
+    sudo mount /dev/mapper/data /data
 }
 
 function lock_data() {
